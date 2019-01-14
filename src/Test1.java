0b94ae342dd6f92e0c233d357840084f85349595
PARESHAN <meshrampriya3@gmail.com>
2019-01-14 17:24:56 +0530

    updated code

--- src/Test1.java
+++ src/Test1.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
 	public static void main(String[] args) {
+		System.out.println("Hello");
+
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PARESHAN\\Desktop\\Selenium\\chromedriver_win32(1)\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
